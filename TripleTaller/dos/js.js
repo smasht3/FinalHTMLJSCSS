43a8d0e1afe1590b9b87a91e2d6130c8586a9d48
Luis Alfonso Pacheco Madera <pachecomaderal0@gmail.com>
2022-05-21 20:35:57 -0500

    Triple Taller

---
+++ TripleTaller/dos/js.js
@@ -0,0 +1,33 @@
+function paress() 
+{
+
+        document.getElementById("par1").style.color = "#ff0000";     
+        document.getElementById("par2").style.color = "#ff0000"; 
+        document.getElementById("par3").style.color = "#ff0000"; 
+        document.getElementById("par4").style.color = "#ff0000"; 
+        document.getElementById("par5").style.color = "#ff0000"; 
+
+        document.getElementById("impar1").style.color = "#ffffff";     
+        document.getElementById("impar2").style.color = "#ffffff"; 
+        document.getElementById("impar3").style.color = "#ffffff"; 
+        document.getElementById("impar4").style.color = "#ffffff"; 
+        document.getElementById("impar5").style.color = "#ffffff"; 
+
+}
+
+function imparess() 
+{
+
+        document.getElementById("impar1").style.color = "#ff0000";     
+        document.getElementById("impar2").style.color = "#ff0000"; 
+        document.getElementById("impar3").style.color = "#ff0000"; 
+        document.getElementById("impar4").style.color = "#ff0000"; 
+        document.getElementById("impar5").style.color = "#ff0000"; 
+
+        document.getElementById("par1").style.color = "#ffffff";     
+        document.getElementById("par2").style.color = "#ffffff"; 
+        document.getElementById("par3").style.color = "#ffffff"; 
+        document.getElementById("par4").style.color = "#ffffff"; 
+        document.getElementById("par5").style.color = "#ffffff"; 
+
+}
---
+++ TripleTaller/uno/js.js
@@ -0,0 +1,13 @@
+function result() {
+     
+	var nombre = document.getElementById('nombre').value;
+	var correo = document.getElementById('correo').value;
+	var asunto = document.getElementById('asunto').value;
+	var mensaje = document.getElementById('mensaje').value;
+
+
+      console.log(nombre);
+      console.log(correo);
+      console.log(asunto);
+      console.log(mensaje);
+    }
